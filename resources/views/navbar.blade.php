a1551dfe54e7cadba64d22b400300c6e812e766b
mariozaton01 <2GDAW12@ARRIAGAINFO.LOCAL>
2021-01-18 12:13:36 +0100

    estructura menus terminado

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -3,7 +3,7 @@
             <button class="btn btn-link btn-sm order-1 order-lg-0 ml-4 " id="sidebarToggle" href="#"><i class="fas fa-bars"></i></button>
             <a class="navbar-brand ml-5 w-100 pl-5" href="portal.blade.php">Vitoria-Gasteiz <br> (Permisos y Obras)</a>
 
-            <!-- Navbar Search-->
+            <!-- Navbar Search
             <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
                 <div class="input-group">
                     <input class="form-control" type="text" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2" />
@@ -12,7 +12,7 @@
                     </div>
                 </div>
             </form>
-            <!-- Navbar-->
+             Navbar-->
             <ul class="navbar-nav ml-auto ml-md-0">
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" id="userDropdown" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
--- resources/views/portal.blade.php
+++ resources/views/portal.blade.php
@@ -14,53 +14,10 @@
     <body class="sb-nav-fixed">
 
     @include("navbar")
+<!--MENU DESPLEGABLE-->
+    @include("menuDesplegable")
 
-    <div id="layoutSidenav">
-        <div id="layoutSidenav_nav">
-            <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
-                <div class="sb-sidenav-menu">
-                    <div class="nav">
-                        <div class="sb-sidenav-menu-heading">Solicitudes</div>
-                        <a class="nav-link" href="portal.blade.php">
-                            <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
-                            Asignar Solicitudes
-                        </a>
-                        <a class="nav-link" href="portal.blade.php">
-                            <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
-                            Comprobar Solicitudes
-                        </a>
-                        <div class="sb-sidenav-menu-heading">Datos</div>
-                        <a class="nav-link " href="#">
-                            <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
-                            Gráficos
-                        </a>
-
-                        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
-                            <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
-                            Tablas
-                            <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                        </a>
-                        <div class="collapse" id="collapsePages" aria-labelledby="headingTwo" data-parent="#sidenavAccordion">
-                            <nav class="sb-sidenav-menu-nested nav accordion" id="sidenavAccordionPages">
-
-                                <a class="nav-link " href="#">
-                                    Empleados
-                                </a>
-                                <a class="nav-link " href="#">
-                                    Usuarios normales
-                                </a>
-
-                            </nav>
-                        </div>
-                    </div>
-                </div>
-                <div class="sb-sidenav-footer">
-                    <div class="small">Logged in as:</div>
-                    Start Bootstrap
-                </div>
-            </nav>
-        </div>
-
+    <!--MAIN-->
             <div id="layoutSidenav_content">
                 <main>
                     <div class="container-fluid">
