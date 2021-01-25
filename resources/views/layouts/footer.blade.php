3120741dda019260e3b3ab5e29d1f754b61111d3
Adrián Pisabarro García <adrian.pisabarro.garcia@gmail.com>
2021-01-25 08:31:40 +0100

    vue

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,12 +1,1 @@
-<footer class="py-4 bg-dark mt-auto">
-    <div class="container-fluid">
-        <div class="d-flex align-items-center justify-content-between small">
-            <div class="text-light">Copyright &copy; Ayuntamiento de Vitoria-Gasteiz 2021</div>
-            <div >
-                <!-- <a href="#" class="text-secondary">Privacy Policy</a>
-                &middot; -->
-                <a href="https://github.com/adrianpisabarrogarcia/permisosobrasvg" target="_blank" class="text-light pr-5">GitHub</a>
-            </div>
-        </div>
-    </div>
-</footer>
+<footer-on-vue id="footer"></footer-on-vue>
