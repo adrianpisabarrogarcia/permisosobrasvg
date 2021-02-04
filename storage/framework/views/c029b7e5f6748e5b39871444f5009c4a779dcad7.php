8f3892001b0ad62aac6fdc13a1580de03155bfbb
Tania Garcia <taniagarcia@MacBook-Air-de-Tania.local>
2021-02-04 23:38:14 +0100

    comentarios

--- storage/framework/views/c029b7e5f6748e5b39871444f5009c4a779dcad7.php
+++ storage/framework/views/c029b7e5f6748e5b39871444f5009c4a779dcad7.php
@@ -1,7 +1,7 @@
 <div class="sb-sidenav-menu-heading bg-primary text-light pt-3">Solicitudes</div>
 <!-- solucionar la ruta -->
 <a class="nav-link enlace" href="<?php echo e(route('comprobarSolicitudes')); ?>">
-    <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt text-primary"></i></div>
+    <div class="sb-nav-link-icon"><i class="fas text-primary fa-list"></i></div>
     Comprobar Solicitudes
 </a>
 <div class="sb-sidenav-menu-heading bg-primary text-light pt-3">Datos</div>
